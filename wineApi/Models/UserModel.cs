5dbc6f600703361e78bdb6f267163532909a8bb2
godOfBugs <godOfBugs@DESKTOP-4J1PEHJ>
2020-11-20 21:39:47 +0300

    Добавил модель пользователя Отредактировал gitignore

---
+++ wineApi/Models/UserModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace wineApi.Models
+{
+    public class UserModel
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string UserEmail { get; set; }
+        public string UserInfo { get; set; }
+        public List<WineModel> UserWines { get; set; }
+    }
+}
